f06feb1eb994e2255bf69a7cf429aafe4fc65ecf
matias delongaro <matiasadelongaro@gmail.com>
2024-03-01 19:55:32 -0300

    mail validacion

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
 // Los primeros 10 turnos dispondrán de un descuento en la entrada.
 // Luego , el valor de las mismas aumentará. (*Luego de que el número de turno llegue al valor 11)
 
-let valorEntradaPrimerPreventa = 5000
+let valorEntrada = 5000
+
 
 function costoTotalInicio(){
     for(let i=1 ; i<10; i++){
         let nombreUsuario = prompt("Ingresa tu Nombre y Apellido");
         
+        
         if(nombreUsuario == ""){
             alert("No ingresaste nombre de usuario");
         }
         else{
-            alert("Hola " + nombreUsuario + "," + "tienes el turno Nº" + i + " que corresponde a la Primer Preventa ($5000). Sólo se venden hasta 5 entradas por usuario.");
+            alert("Hola " + nombreUsuario + "," + "tienes el Nº de orden" + i + " Recuerda que sólo se venden hasta 2 entradas por usuario.");
             let num = Number(prompt("Ingrese la cantidad de entradas que va a solicitar"));{
-                if(num>5){
-                    alert("El usuario sólo puede comprar hasta 5 entradas")
+                if(num>2){
+                    alert("El usuario sólo puede comprar hasta 2 entradas")
                 }
                 else if(num == ""){
                     alert("Ingrese la cantidad de entradas que va a solicitar")
                 }
                 else{
-                    alert("Has seleccionado la cantidad de " + num + " entradas. El total a pagar es de "  + num * valorEntradaPrimerPreventa + " pesos.")
+                    alert("Has seleccionado la cantidad de " + num + " entradas. El total a pagar es de "  + num * valorEntrada + " pesos.");
+                    let mail = prompt('Ingresa tu Email');
+                    if(mail == ""){
+                        alert("Debes ingresar tu mail, allí te llegaran las entradas. Por razones de seguridad deberás comenzar nuevamente el proceso");
+                    }
+                    else{
+                        document.write('<div class="container"><div class="figure"><h1>Felicitaciones, tu compra se ha realizado con éxito.</h1><p>Recibiras un mail en donde se te solicitará el numero de orden y se te indicaran los pasos a seguir.</p></div></div>');
+                    }
+                   
+                    
                 }
-            }
+            } 
             {break};
+            
+            
             
         }
 }
@@ -31,46 +44,5 @@ function costoTotalInicio(){
 
 }
 costoTotalInicio()
-
-let valorEntradaSegundaPreventa = 7000
-
-function costoTotal(){
-    for(let i=11 ; i<100; i++){
-        let nombreUsuario = prompt("Ingresa tu Nombre y Apellido");
-        
-        if(nombreUsuario == ""){
-            alert("No ingresaste nombre de usuario");
-        }
-        else{
-            alert("Hola " + nombreUsuario + "," + "tienes el turno Nº" + i + " que corresponde a la Segunda Preventa ($7000). Sólo se venden hasta 5 entradas por usuario. ");
-            let num = Number(prompt("Ingrese la cantidad de entradas que va a solicitar"));{
-                if(num>5){
-                    alert("El usuario sólo puede comprar hasta 5 entradas")
-                }
-                else if(num == ""){
-                    alert("Ingrese la cantidad de entradas que va a solicitar")
-                }
-                else{
-                    alert("Has seleccionado la cantidad de " + num + " entradas. El total a pagar es de "  + num * valorEntradaSegundaPreventa + " pesos.")
-                }
-            }
-           {break};
-           
-            
-        }
-}
-
-
-}
-costoTotal()
-
-
-
-
-
-
-
-
-
 
 
